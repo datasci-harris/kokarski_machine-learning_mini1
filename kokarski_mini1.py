8a3663dad5e34da2284810143714e96db161415f
kokarski <kokarski@uchicago.edu>
2022-04-18 23:27:17 -0500

    clean code

--- kokarski_mini1.py
+++ kokarski_mini1.py
@@ -13,46 +13,50 @@ import numpy as np
 ipums = pd.read_csv("Documents/GitHub/kokarski_machine-learning_mini1/usa_00003.csv")
 crosswalk = pd.read_csv("Documents/GitHub/kokarski_machine-learning_mini1/PPHA_30545_MP01-Crosswalk.csv")
 
-# (2) variable creation (a)
-# mapping categorical education variable to continuous using crosswalk dataset
-newipums = ipums.assign(EDUCDC=0)
-newipums = newipums.assign(HSDIP=0)
-newipums = newipums.assign(CSDIP=0)
-newipums = newipums.assign(BLACK=0)
-newipums = newipums.assign(WHITE=0)
-newipums = newipums.assign(HISPANIC=0)
-newipums = newipums.assign(MARRIED=0)
-newipums = newipums.assign(FEMALE=0)
-newipums = newipums.assign(VET=0)
+def variablecreation():
 
-for x in range(0, len(newipums.EDUCDC)):
-    tempeduc = newipums.EDUCD[x]
-    temppos = crosswalk[crosswalk["educd"] == tempeduc].index.tolist()[0]
-    newipums.EDUCDC[x] = crosswalk.educdc[temppos]
-   
-# creating dummy variables:
-# hsdip (063 or 063)
-    if(newipums.EDUCD[x] == 63 or newipums.EDUCD[x] == 64):
-        newipums.HSDIP[x] = 1
-# csdip 101 and 116 (inclusive)
-    if(newipums.EDUCD[x] >= 101 and newipums.EDUCD[x] <= 116):
-        newipums.CSDIP[x] = 1
-# 1= white, 2= black
-    if(newipums.RACE[x] == 1):
-        newipums.WHITE[x] = 1
-    if(newipums.RACE[x] == 2):
-        newipums.BLACK[x] = 2
-# 0 if not hispanic >0 if hispanic
-    if(newipums.HISPAN[x] > 0):
-        newipums.HISPANIC[x] = 1
-# between 1 and 3 are married (inlcusive)
-    if(newipums.MARST[x] >= 1 and newipums.MARST[x] <= 3):
-        newipums.MARRIED[x] = 1
-# sex=2 is female
-    if(newipums.SEX[x] == 2):
-        newipums.FEMALE[x] = 1
-# vet status=2 are a vet
-    if(newipums.VETSTAT == 2):
-        newipums.VET[x] = 1
-        
-        
+    newipums = ipums.assign(EDUCDC=0)
+    newipums = newipums.assign(HSDIP=0)
+    newipums = newipums.assign(CSDIP=0)
+    newipums = newipums.assign(BLACK=0)
+    newipums = newipums.assign(WHITE=0)
+    newipums = newipums.assign(HISPANIC=0)
+    newipums = newipums.assign(MARRIED=0)
+    newipums = newipums.assign(FEMALE=0)
+    newipums = newipums.assign(VET=0)
+    for x in range(0, len(newipums.EDUCDC)):
+        # mapping categorical education to continuous using crosswalk dataset
+        tempeduc = newipums.EDUCD[x]
+        temppos = crosswalk[crosswalk["educd"] == tempeduc].index.tolist()[0]
+        newipums.EDUCDC[x] = crosswalk.educdc[temppos]
+        # creating dummy variables:
+        # hsdip (063 or 063)
+        if(newipums.EDUCD[x] == 63 or newipums.EDUCD[x] == 64):
+            newipums.HSDIP[x] = 1
+        # csdip 101 and 116 (inclusive)
+        if(newipums.EDUCD[x] >= 101 and newipums.EDUCD[x] <= 116):
+            newipums.CSDIP[x] = 1
+        # 1= white, 2= black
+        if(newipums.RACE[x] == 1):
+            newipums.WHITE[x] = 1
+        if(newipums.RACE[x] == 2):
+            newipums.BLACK[x] = 2
+        # 0 if not hispanic >0 if hispanic
+        if(newipums.HISPAN[x] > 0):
+            newipums.HISPANIC[x] = 1
+        # between 1 and 3 are married (inlcusive)
+        if(newipums.MARST[x] >= 1 and newipums.MARST[x] <= 3):
+            newipums.MARRIED[x] = 1
+        # sex=2 is female
+        if(newipums.SEX[x] == 2):
+            newipums.FEMALE[x] = 1
+        # vet status=2 then they are a vet
+        if(newipums.VETSTAT[x] == 2):
+            newipums.VET[x] = 1
+        # modifying age and incwage
+    newipums.AGE = np.square(newipums.AGE)
+    newipums.INCWAGE = np.log(newipums.INCWAGE)
+    return(newipums)
+
+
+newdf = variablecreation()
